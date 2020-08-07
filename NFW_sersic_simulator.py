1e6d7717d3065117149d825a6406d5a583b676b6
hopkins9942 <matthew.hopkins9942@gmail.com>
2020-08-07 10:54:49 +0100

    v2, all tested

--- NFW_sersic_simulator.py
+++ NFW_sersic_simulator.py
@@ -11,40 +11,45 @@ To add:
     save positions of lensed images
 """
 
-#Configuration
+# Configuration
 from autoconf import conf
 import os
 import sys
 
-workspace_path = "/home/matthew/Durham2020/AutoLensWorkspace/autolens_workspace/" 
+workspace_path = "/home/matthew/Durham2020/AutoLensWorkspace/autolens_workspace/"
 
-#Params - from command line arguments
-sersic_elliptical_comps_0=float(sys.argv[1])
-sersic_elliptical_comps_1=float(sys.argv[2])
-NFW_mass=float(sys.argv[3])
-einstein_radius=float(sys.argv[4])
-source_centre_0=float(sys.argv[5])
-source_centre_1=float(sys.argv[6])
-source_elliptical_comps_0=float(sys.argv[7])
-source_elliptical_comps_1=float(sys.argv[8])
-source_radius=float(sys.argv[9])
+# Params - from command line arguments
+sersic_elliptical_comps_0 = float(sys.argv[1])
+sersic_elliptical_comps_1 = float(sys.argv[2])
+NFW_mass = float(sys.argv[3])
+einstein_radius = float(sys.argv[4])
+source_centre_0 = float(sys.argv[5])
+source_centre_1 = float(sys.argv[6])
+source_elliptical_comps_0 = float(sys.argv[7])
+source_elliptical_comps_1 = float(sys.argv[8])
+source_radius = float(sys.argv[9])
 
 sersic_elliptical_comps = (sersic_elliptical_comps_0, sersic_elliptical_comps_1)
 source_centre = (source_centre_0, source_centre_1)
 source_elliptical_comps = (source_elliptical_comps_0, source_elliptical_comps_1)
 
 
+dataset_name = "_".join(
+    (
+        str(sersic_elliptical_comps),
+        str(NFW_mass),
+        str(einstein_radius),
+        str(source_centre),
+        str(source_elliptical_comps),
+        str(source_radius),
+    )
+)
 
-
-dataset_name = "_".join((
-                      str(sersic_elliptical_comps),
-                      str(NFW_mass),
-                      str(einstein_radius),
-                      str(source_centre),
-                      str(source_elliptical_comps),
-                      str(source_radius)))
-
-save_path= "/home/matthew/Durham2020/MyScripts/autolens_tools/ExampleData/NFW_sersic/" + dataset_name + "/"
+save_path = (
+    "/home/matthew/Durham2020/MyScripts/autolens_tools/ExampleData/NFW_sersic/"
+    + dataset_name
+    + "/"
+)
 
 conf.instance = conf.Config(
     config_path=f"{workspace_path}config/", output_path=save_path,
@@ -54,7 +59,7 @@ if not os.path.exists(save_path):
     os.makedirs(save_path)
 
 
-#Script
+# Script
 import autolens as al
 import autolens.plot as aplt
 import datetime
@@ -66,22 +71,24 @@ print(datetime.datetime.now())
 redshift_lens = 0.5
 redshift_source = 1.0
 
-#Creating profiles and galaxies:
-dark_profile=al.mp.SphericalTruncatedNFWMCRLudlow(
-        centre = (0.0, 0.0),
-        mass_at_200 = 10**NFW_mass,
-        redshift_object = redshift_lens,
-        redshift_source = redshift_source,
-    )
+# Creating profiles and galaxies:
+dark_profile = al.mp.SphericalTruncatedNFWMCRLudlow(
+    centre=(0.0, 0.0),
+    mass_at_200=10 ** NFW_mass,
+    redshift_object=redshift_lens,
+    redshift_source=redshift_source,
+)
+
 
 def sersic_profile(intensity):
     return al.mp.EllipticalSersic(
-        centre = (0.0, 0.0),
-        elliptical_comps = sersic_elliptical_comps,
-        effective_radius = 1.2,
-        sersic_index = 4,
-        intensity = intensity,
-        )
+        centre=(0.0, 0.0),
+        elliptical_comps=sersic_elliptical_comps,
+        effective_radius=1.2,
+        sersic_index=4,
+        intensity=intensity,
+    )
+
 
 def intensity_setter(target_e_radius):
     """
@@ -89,14 +96,20 @@ def intensity_setter(target_e_radius):
     """
     print("\nsetting intensity:")
     print(datetime.datetime.now())
-    
+
     def func(intensity):
-        value = al.Galaxy(0.5, sersic=sersic_profile(intensity), dark=dark_profile).einstein_radius_in_units() - target_e_radius
+        value = (
+            al.Galaxy(
+                0.5, sersic=sersic_profile(intensity), dark=dark_profile
+            ).einstein_radius_in_units()
+            - target_e_radius
+        )
         print(f"Intensity: {intensity}")
         print(f"Value: {value}")
         return value
-    
+
     return fsolve(func, 0.1, xtol=1e-3)
+
 
 lens_galaxy = al.Galaxy(
     redshift=redshift_lens,
@@ -117,10 +130,7 @@ source_galaxy = al.Galaxy(
 # Creating Tracer and Simualtor, then writing data
 tracer = al.Tracer.from_galaxies(galaxies=[lens_galaxy, source_galaxy])
 tracer.save(file_path=save_path, filename="true_tracer")
-grid = al.Grid.uniform(
-    shape_2d=(200, 200), 
-    pixel_scales=0.05,
-)
+grid = al.Grid.uniform(shape_2d=(200, 200), pixel_scales=0.05,)
 
 
 print("\nplotting observed image")
@@ -134,7 +144,7 @@ simulator = al.SimulatorImaging(
     background_sky_map=al.Array.full(fill_value=0.1, shape_2d=grid.shape_2d),
     add_noise=True,
 )
-imaging = simulator.from_tracer_and_grid(tracer=tracer, grid=grid, name = dataset_name)
+imaging = simulator.from_tracer_and_grid(tracer=tracer, grid=grid, name=dataset_name)
 imaging.output_to_fits(
     image_path=f"{save_path}image.fits",
     psf_path=f"{save_path}psf.fits",
@@ -142,5 +152,7 @@ imaging.output_to_fits(
     overwrite=True,
 )
 
-observed_plotter = aplt.Plotter(output=aplt.Output(path = save_path, filename="ObservedImage", format = "png"))
-aplt.Imaging.image(imaging,plotter=observed_plotter)
+observed_plotter = aplt.Plotter(
+    output=aplt.Output(path=save_path, filename="ObservedImage", format="png")
+)
+aplt.Imaging.image(imaging, plotter=observed_plotter)
